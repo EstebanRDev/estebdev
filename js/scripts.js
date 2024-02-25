12501bcd536849548e1fcb7f01e2110007122136
pythonibm2023 <wiriascos@poligran.edu.co>
2024-02-24 21:31:58 -0500

    update load

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,13 @@
 
 //load
 
-window.onload = function(){
-    $('#onload').fadeOut();
-    $('body').removeClass('hidden');
-}
+window.onload = function() {
+    setTimeout(function() {
+        $('#onload').fadeOut();
+        $('body').removeClass('hidden');
+    }, 1000); // 5000 milisegundos = 5 segundos
+};
+
 
 
 
